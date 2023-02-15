8636dc61709a4542d345d250ad109ab8c5eaac04
Tom Scott <tom.scott45456@gmail.com>
2023-02-15 00:25:46 +0000

    Generate vertices within bounds
    The tesselation can be set within the bounds of a specified width

--- Assets/Scripts/Terrain/TerrainMeshData.cs
+++ Assets/Scripts/Terrain/TerrainMeshData.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 class TerrainMeshData : MonoBehaviour
 {
     [SerializeField] private int m_meshResolution;
+    [SerializeField] private float m_width;
 
     public int GetResolution()
     {
         return m_meshResolution;
+    }
+
+    public float GetWidth()
+    {
+        return m_width;
     }
 }
 
@@ -23,14 +29,24 @@ public class MeshGenerator : Editor
         Mesh mesh = m_terrainMeshData.GetComponent<MeshFilter>().sharedMesh;
 
         int resolution = m_terrainMeshData.GetResolution();
+        float width = m_terrainMeshData.GetWidth();
 
         Vector3[] vertices = new Vector3[resolution * resolution];
+
+        // res-1 because we start counting at 0!
+        // Caching aspect here because we don't wanna be calculating it 
+        // max 65,536 times! 
+        float aspect = width / (resolution - 1);
 
         for (int y = 0; y < resolution; y++)
         {
             for (int x = 0; x < resolution; x++)
             {
-                vertices[x * resolution + y] = new Vector3(x, 0, y);
+                vertices[x * resolution + y] = new Vector3(
+                    aspect * x,
+                    0,
+                    aspect * y
+                );
             }
         }
 
@@ -45,6 +61,7 @@ public class MeshGenerator : Editor
                 int i2 = (y + 1) * resolution + x;
                 int i3 = (y + 1) * resolution + x + 1;
 
+                // Counter-Clockwise triangle indices
                 triangles[index++] = i0;
                 triangles[index++] = i1;
                 triangles[index++] = i2;

bb73952e44a60052934ef33d2059b93bd3d8f84c
Tom Scott <tom.scott45456@gmail.com>
2023-03-02 14:58:57 +0000

    Init IK
    Set up the bone lengths and the bone transforms into arrays for reference later

--- Assets/Scripts/InverseKinematic.cs
+++ Assets/Scripts/InverseKinematic.cs
@@ -16,16 +16,44 @@ public class InverseKinematic : MonoBehaviour
 
     [SerializeField] private float m_snapBackStrength;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
+    private float[] m_boneLengths;
+    private float m_completeLength;
+    private Transform[] m_bones;
+    private Vector3[] m_positions;
 
+    private void Awake()
+    {
+        Init();
     }
 
     // Update is called once per frame
     private void Update()
     {
 
+    }
+
+    private void Init()
+    {
+        m_bones = new Transform[m_chainLength + 1];
+        m_positions = new Vector3[m_chainLength + 1];
+        m_boneLengths = new float[m_chainLength];
+
+        m_completeLength = 0f;
+
+        // Calculate the lengths between the bones 
+        Transform currentTransform = transform;
+        for (int i = m_chainLength; i >= 0; --i)
+        {
+            m_bones[i] = currentTransform;
+
+            if (i != m_chainLength)
+            {
+                m_boneLengths[i] = (m_bones[i + 1].position - currentTransform.position).magnitude;
+                m_completeLength *= m_boneLengths[i];
+            }
+
+            currentTransform = currentTransform.parent;
+        }
     }
 
     private void OnDrawGizmos()

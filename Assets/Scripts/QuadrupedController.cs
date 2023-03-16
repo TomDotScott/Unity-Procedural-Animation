75b08cfc2c4abeb7a92b6e290bd93ac1a2e33e24
Tom Scott <tom.scott45456@gmail.com>
2023-03-16 11:31:01 +0000

    Smooth head rotation
    Using SLERP

--- Assets/Scripts/QuadrupedController.cs
+++ Assets/Scripts/QuadrupedController.cs
@@ -7,10 +7,19 @@ public class QuadrupedController : MonoBehaviour
     [SerializeField] private Transform m_eyeTarget;
     [SerializeField] private Transform m_headBone;
 
+    [SerializeField] private float m_headTurnSpeed;
+
     private void LateUpdate()
     {
         Vector3 towardTarget = m_eyeTarget.position - m_headBone.position;
 
-        m_headBone.rotation = Quaternion.LookRotation(towardTarget, transform.up);
+        Quaternion targetRotation = Quaternion.LookRotation(towardTarget, transform.up);
+
+        // Smooth the rotation
+        m_headBone.rotation = Quaternion.Slerp(
+            targetRotation,
+            m_headBone.rotation,
+            1 - Mathf.Exp(-m_headTurnSpeed * Time.deltaTime)
+       );
     }
 }

c52d1f8df13d0ad2b59f73729d07caba2864b62b
Tom Scott <tom.scott45456@gmail.com>
2023-03-16 11:37:28 +0000

    Limit head rotation angle

--- Assets/Scripts/QuadrupedController.cs
+++ Assets/Scripts/QuadrupedController.cs
@@ -8,17 +8,27 @@ public class QuadrupedController : MonoBehaviour
     [SerializeField] private Transform m_headBone;
 
     [SerializeField] private float m_headTurnSpeed;
+    [SerializeField] private float m_maxHeadAngle;
 
     private void LateUpdate()
     {
-        Vector3 towardTarget = m_eyeTarget.position - m_headBone.position;
+        Quaternion currentRotation = m_headBone.localRotation;
 
-        Quaternion targetRotation = Quaternion.LookRotation(towardTarget, transform.up);
+        // reset the headbone rotation 
+        m_headBone.localRotation = Quaternion.identity;
+
+        Vector3 towardTargetWorldSpace = m_eyeTarget.position - m_headBone.position;
+        Vector3 towardTargetLocalSpace = m_headBone.InverseTransformDirection(towardTargetWorldSpace);
+
+        // Apply the rotation limit to the head
+        towardTargetLocalSpace = Vector3.RotateTowards(Vector3.forward, towardTargetLocalSpace, Mathf.Deg2Rad * m_maxHeadAngle, 0f);
+
+        Quaternion targetRotation = Quaternion.LookRotation(towardTargetLocalSpace, transform.up);
 
         // Smooth the rotation
         m_headBone.rotation = Quaternion.Slerp(
+            currentRotation,
             targetRotation,
-            m_headBone.rotation,
             1 - Mathf.Exp(-m_headTurnSpeed * Time.deltaTime)
        );
     }

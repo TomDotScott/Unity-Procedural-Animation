cc56bd718195cb475f6dd1fc9194d516e8c04d33
Tom Scott <tom.scott45456@gmail.com>
2023-04-30 20:01:57 +0100

    Bipedal walk
    By projecting the centre of mass onto an ellipse, the biped walks if it is outside the area

--- Assets/Scripts/BipedalController.cs
+++ Assets/Scripts/BipedalController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,13 @@ public class BipedalController : MonoBehaviour
     [SerializeField] private Stepper m_leftLeg;
     [SerializeField] private Stepper m_rightLeg;
 
+    [SerializeField] private float m_balancingMajorRadius;
+    [SerializeField] private float m_balancingMinorRadius;
+
+    [SerializeField] private LineRenderer m_lineRenderer;
+
+    private Vector3 m_lastBodyPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,24 +26,110 @@ public class BipedalController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
+
+    private bool IsBalanced()
+    {
+        // Project our targets onto an ellipse and then work out if we are inside or outside of it
+        Vector3 ellipseCentre = (m_leftLeg.transform.position + m_rightLeg.transform.position) / 2f;
+        ellipseCentre = Vector3.ProjectOnPlane(ellipseCentre, Vector3.up);
+
+        Vector2 ellipseCentre2D = new Vector2(ellipseCentre.x, ellipseCentre.z);
+
+        Vector3 point = Vector3.ProjectOnPlane(transform.position, Vector3.up);
+        Vector3 point2D = new Vector2(point.x, point.z);
+
+        Vector3 footAxis = Vector3.ProjectOnPlane(m_rightLeg.transform.position - m_leftLeg.transform.position, transform.up).normalized;
+        Vector2 footAxis2D = new Vector2(footAxis.x, footAxis.z);
+
+        Vector2 rotatedPoint = MathUtils.Rotate(point2D, MathUtils.GetAngle2D(footAxis2D, Vector2.right));
+
+        m_lineRenderer.SetPositions(CalculateEllipse());
+        m_lineRenderer.gameObject.transform.rotation = new Quaternion(0, MathUtils.GetAngle2D(point, Vector2.right), 0, 0);
+
+        return MathUtils.IsPointInEllipse(rotatedPoint, ellipseCentre2D, m_balancingMinorRadius, m_balancingMajorRadius);
+    }
+
+
+    private Vector3[] CalculateEllipse()
+    {
+        Vector3[] ellipsePoints = new Vector3[100];
+
+        for (int i = 0; i < ellipsePoints.Length; i++)
+        {
+            float angle = (i / 100f) * 360f * Mathf.Deg2Rad;
+
+            float x = Mathf.Sin(angle) * m_balancingMinorRadius;
+            float y = Mathf.Cos(angle) * m_balancingMajorRadius;
+
+            ellipsePoints[i] = new Vector3(x, m_leftLeg.transform.position.y, y);
+        }
+
+        return ellipsePoints;
+    }
+
 
     private IEnumerator UpdateLegMovement()
     {
+        bool moving = false;
+        bool moveLeft = false;
         while (true)
         {
-            if (!m_leftLeg.Moving)
+            if (!moving)
             {
-                m_rightLeg.AttemptMove();
-            }
+                if (!IsBalanced())
+                {
+                    Vector3 velocity = transform.position - m_lastBodyPosition;
+                    Vector3 centreOfMass = transform.position;
 
-            if (!m_rightLeg.Moving)
+                    float leftDistance = Vector3.Distance(m_leftLeg.transform.position, centreOfMass);
+                    float rightDistance = Vector3.Distance(m_rightLeg.transform.position, centreOfMass);
+
+                    if (leftDistance >= rightDistance)
+                    {
+                        moving = true;
+                        moveLeft = true;
+                    }
+                    else
+                    {
+                        moving = true;
+                        moveLeft = false;
+                    }
+                }
+            }
+            else
             {
-                m_leftLeg.AttemptMove();
+                if (moveLeft)
+                {
+                    if (!m_rightLeg.Moving)
+                    {
+                        m_leftLeg.AttemptMove();
+                        moving = m_leftLeg.Moving;
+                    }
+                }
+                else
+                {
+                    if (!m_leftLeg.Moving)
+                    {
+                        m_rightLeg.AttemptMove();
+                        moving = m_rightLeg.Moving;
+                    }
+                }
             }
 
             yield return null;
         }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = IsBalanced() ? Color.green : Color.red;
+
+
+        Gizmos.DrawWireSphere(m_leftLeg.transform.position, 0.2f);
+        Gizmos.DrawWireSphere(m_rightLeg.transform.position, 0.2f);
+
+        Debug.DrawLine(transform.position, transform.position + transform.up * 2f);
     }
 }
---
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+class MathUtils
+{
+    public static Vector2 Rotate(Vector2 v, float delta)
+    {
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+
+    public static float GetAngle2D(Vector2 a, Vector2 b)
+    {
+        float dot = Vector2.Dot(a, b);
+        float mags = a.magnitude * b.magnitude;
+
+        return Mathf.Acos(dot / mags);
+    }
+
+    public static bool IsPointInEllipse(Vector2 point, Vector2 ellipseCentre, float minorRadius, float majorRadius)
+    {
+        float major = Mathf.Pow((point.x - ellipseCentre.x) / majorRadius, 2f);
+
+        float minor = Mathf.Pow((point.y - ellipseCentre.y) / minorRadius, 2f);
+
+        return major + minor <= 1f;
+    }
+
+}

07af91a3ad1b352f57377f7b390a1b9717023815
Tom Scott <tom.scott45456@gmail.com>
2023-05-01 02:58:07 +0100

    Throw stick on click

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,46 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float m_movementSpeed = 5.0f;
     [SerializeField] private float m_mouseSensitivity = 3.0f;
 
+    [SerializeField] private Transform m_stickHoldTransform;
+
     private float m_verticalRotation;
     private float m_horizontalRotation;
 
-    void Update()
+    [SerializeField] private GameObject m_stick;
+    private bool m_hasStick = true;
+    [SerializeField] private float m_throwForce;
+    [SerializeField] private float m_throwUpwardForce;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && m_hasStick)
+        {
+            ThrowStick();
+        }
+
+        if (m_hasStick)
+        {
+            m_stick.transform.SetPositionAndRotation(m_stickHoldTransform.position, m_stickHoldTransform.rotation);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+    }
+
+    private void ThrowStick()
+    {
+        m_hasStick = false;
+
+        Rigidbody stickRb = m_stick.GetComponent<Rigidbody>();
+
+        Vector3 force = Camera.main.transform.forward * m_throwForce + transform.up * m_throwUpwardForce;
+        stickRb.AddForce(force, ForceMode.Impulse);
+    }
+
+
+    private void Move()
     {
         // Mouse rotation
         m_horizontalRotation += Input.GetAxis("Mouse X") * m_mouseSensitivity;
